5081c3c78df216b8d67a6ad29eb00b4286c08e74
pkarlstrand <64445912+pkarlstrand@users.noreply.github.com>
2020-05-14 17:37:48 -0500

    Update Radio.js

--- Radio.js
+++ Radio.js
@@ -15,30 +15,3 @@ const Radio = props => {
 };
 export default Radio;
 
-/*
-import React from "react";
-
-class Radio extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedOption: ""
-    };
-
-    this.radioChange = this.radioChange.bind(this);
-  }
-
-  radioChange(e) {
-    this.setState({
-      selectedOption: e.currentTarget.value
-    });
-  }
-
-  render() {
-    return null;
-
-    
-  }
-}
-export default Radio;
-*/
